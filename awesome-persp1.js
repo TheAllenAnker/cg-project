e5e7d84e4c1397ba15e31d4dde657cec9e20f600
TheAllenAnker <theallenanker@gmail.com>
2018-12-16 16:02:36 +0800

    Finally got the texture to work.

--- awesome-persp1.js
+++ awesome-persp1.js
@@ -11,16 +11,24 @@ var gl;
 
 var pointsArray = [];//存放 按每面两个三角形6个顶点顺序放的顶点位置 数组
 var normalsArray = [];//存放 按每面两个三角形6个顶点顺序放的顶点法向量 数组
+var textureCoordsArray = [];
 
 var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5, -0.5, -0.5, 1.0 )
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0)
+];
+
+var textureCoord = [
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
 ];
 
 // for (let i = 0; i < 8; i++) {
@@ -29,15 +37,15 @@ var vertices = [
 //     }
 // }
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );//A=0表示无穷远光及方向，A=1表示点光源及位置
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);//A=0表示无穷远光及方向，A=1表示点光源及位置
 
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
-var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
-var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
-var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
-var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
+var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
 var materialShininess = 100.0;
 
 //var ctm;//后面代码并没有用到？？？
@@ -88,7 +96,7 @@ var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
 var axis = 0;
-var thetaRotate =[0, 0, 0];
+var thetaRotate = [0, 0, 0];
 var thetaLoc;
 
 var flag = false;//Toggle Rotation 开启结束旋转
@@ -105,64 +113,90 @@ function quad(a, b, c, d) {
 
     pointsArray.push(vertices[a]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[0]);
+
     pointsArray.push(vertices[b]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[1]);
+
     pointsArray.push(vertices[c]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[2]);
+
     pointsArray.push(vertices[a]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[0]);
+
     pointsArray.push(vertices[c]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[2]);
+
     pointsArray.push(vertices[d]);
     normalsArray.push(normal);
+    textureCoordsArray.push(textureCoord[3]);
 }
 
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+function colorCube() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
 }
-
 
 
 window.onload = function init() {
     //============init WebGL
-    canvas = document.getElementById( "gl-canvas" );
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
 
 
     //=========Load shaders and initialize attribute buffers
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var image = new Image();
+    var texture = gl.createTexture();
+    image.src = "panda.png";
+    image.onload = function () {
+        configureTexture(texture, image);
+    };
 
     colorCube();//立方体的所有顶点位置和法向量写入缓存
 
     var nBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal );
+    var vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );//顶点位置数组pointsArray写入缓存vBuffer缓存
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);//顶点位置数组pointsArray写入缓存vBuffer缓存
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);                     //vBuffer缓存同SHADER中的顶点属性vPosition，建立关联
+
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(textureCoordsArray), gl.STATIC_DRAW);
+
+    var aTextCoords = gl.getAttribLocation(program, "aTextCoords");
+    gl.vertexAttribPointer(aTextCoords, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aTextCoords);
 
     var ambientProduct = mult(lightAmbient, materialAmbient);//计算出Ia*Ka
     var diffuseProduct = mult(lightDiffuse, materialDiffuse);//计算出Id*Kd
@@ -170,10 +204,10 @@ window.onload = function init() {
 
 
     gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));//传递环境反射向量
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );//传递漫反射向量
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"),flatten(specularProduct) );//传递镜面反射向量
-    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"),flatten(lightPosition) );//传递光源位置向量
-    gl.uniform1f(gl.getUniformLocation(program, "shininess"),materialShininess);//传递镜面反射的高光系数
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));//传递漫反射向量
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));//传递镜面反射向量
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));//传递光源位置向量
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);//传递镜面反射的高光系数
 
     thetaLoc = gl.getUniformLocation(program, "theta"); //获取shader变量theta位置
 
@@ -186,10 +220,18 @@ window.onload = function init() {
     p2.innerHTML = "Cartesian&nbsp;eye(x,y,z): " + x + ", " + y + ", " + z;
 
     //交互控制，获取旋转轴选项X,Y,Z参数， 和是否继续旋转开关参数
-    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
-    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
-    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+    document.getElementById("ButtonX").onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById("ButtonY").onclick = function () {
+        axis = yAxis;
+    };
+    document.getElementById("ButtonZ").onclick = function () {
+        axis = zAxis;
+    };
+    document.getElementById("ButtonT").onclick = function () {
+        flag = !flag;
+    };
 
     document.getElementById("width").onchange = function (event) {
         width = event.target.value;
@@ -226,7 +268,7 @@ window.onload = function init() {
         p2.innerHTML = "eye(x,y,z): " + x + ", " + y + ", " + z;
     };
     // keydown listener for the keyboard events
-    document.onkeydown = function(){
+    document.onkeydown = function () {
         // 'O' keydown
         if (event.keyCode === 79) {
             isOrtho = true;
@@ -275,20 +317,20 @@ window.onload = function init() {
     render();
 };
 
-var render = function(){
+var render = function () {
 
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     eye = vec3(radius * Math.sin(theta) * Math.cos(phi),
         radius * Math.sin(theta) * Math.sin(phi),
         radius * Math.cos(theta));
 
-    if(flag) thetaRotate[axis] += 2.0;
+    if (flag) thetaRotate[axis] += 2.0;
 
     modelViewMatrix = lookAt(eye, at, up);
-    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[xAxis], [1, 0, 0] ));
-    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[yAxis], [0, 1, 0] ));
-    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[zAxis], [0, 0, 1] ));
+    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[xAxis], [1, 0, 0]));
+    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[yAxis], [0, 1, 0]));
+    modelViewMatrix = mult(modelViewMatrix, rotate(thetaRotate[zAxis], [0, 0, 1]));
 
     scaleMatrix = scalem(width, height, depth);
     if (isOrtho) {
@@ -297,10 +339,22 @@ var render = function(){
         projectionMatrix = perspective(fovy, aspect, near, far);
     }
 
-    gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),  false, flatten(projectionMatrix));//传投影变换矩阵
-    gl.uniformMatrix4fv( gl.getUniformLocation(program,"modelViewMatrix"), false, flatten(modelViewMatrix) );//传递旋转变换矩阵，即模视矩阵
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));//传投影变换矩阵
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));//传递旋转变换矩阵，即模视矩阵
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "scaleMatrix"), false, flatten(scaleMatrix));
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );//启动shader绘制三角形，numVertices初始化为36（6*6=36个三角形顶点）
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);//启动shader绘制三角形，numVertices初始化为36（6*6=36个三角形顶点）
 
     requestAnimFrame(render);//双帧切换
 };
+
+function configureTexture(texture, image) {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    gl.uniform1i(gl.getUniformLocation(program, "uTextureSampler"), 0);
+}
