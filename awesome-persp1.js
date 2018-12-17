2e194266cf1774770370a21638259dd510bf2c2f
TheAllenAnker <theallenanker@gmail.com>
2018-12-17 19:35:17 +0800

    Add another object and got the ortho/persp shift to work.

--- awesome-persp1.js
+++ awesome-persp1.js
@@ -23,15 +23,27 @@ var vertices = [
     vec4(0.5, 0.5, -0.5, 1.0),
     vec4(0.5, -0.5, -0.5, 1.0),
 
-    vec4(-0.5, -0.5, 0.5, 0.8),
-    vec4(-0.5, 0.5, 0.5, 0.8),
-    vec4(0.5, 0.5, 0.5, 0.8),
-    vec4(0.5, -0.5, 0.5, 0.8),
-    vec4(-0.5, -0.5, -0.5, 0.8),
-    vec4(-0.5, 0.5, -0.5, 0.8),
-    vec4(0.5, 0.5, -0.5, 0.8),
-    vec4(0.5, -0.5, -0.5, 0.8)
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0)
 ];
+
+var vertexColors = [
+    vec4(0.0, 0.0, 0.0, 1.0),  // black
+    vec4(1.0, 0.0, 0.0, 1.0),  // red
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4(0.0, 1.0, 0.0, 1.0),  // green
+    vec4(0.0, 0.0, 1.0, 1.0),  // blue
+    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+    vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+    vec4(1.0, 1.0, 1.0, 1.0),  // white
+];
+
 // set the coordinates for the two objects
 for (let i = 0; i < 8; i++) {
     vertices[i][0] -= 1.5;
@@ -40,6 +52,11 @@ for (let i = 0; i < 8; i++) {
 for (let i = 8; i < 16; i++) {
     vertices[i][0] += 1.0;
 }
+
+var va = vec4(0.0, 0.0, -1.0, 1);
+var vb = vec4(0.0, 0.942809, 0.333333, 1);
+var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+var vd = vec4(0.816497, -0.471405, 0.333333, 1);
 
 var textureCoord = [
     vec2(0, 0),
@@ -79,7 +96,7 @@ const up = vec3(0, 1, 0);
 
 
 //平行投影：
-const near = -100;
+const near = 0.3;
 const far = 100;
 const left = -1;
 const right = 1;
@@ -113,6 +130,56 @@ var thetaLoc;
 var flag = false;//Toggle Rotation 开启结束旋转
 var fogFlag = false; // fog control
 var scrollFlag = true; // prevent the page from scrolling when the mouse wheel is scrolling
+
+var numTimesToSubdivide = 3;
+var index = 72;
+
+function triangle(a, b, c) {
+
+    pointsArray.push(a);
+    pointsArray.push(b);
+    pointsArray.push(c);
+
+    // normals are vectors
+    normalsArray.push(a[0], a[1], a[2], 0.0);
+    normalsArray.push(b[0], b[1], b[2], 0.0);
+    normalsArray.push(c[0], c[1], c[2], 0.0);
+
+    textureCoordsArray.push(vertexColors[5]);
+    textureCoordsArray.push(vertexColors[6]);
+    textureCoordsArray.push(vertexColors[7]);
+
+    index += 3;
+}
+
+
+function divideTriangle(a, b, c, count) {
+    if (count > 0) {
+
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        ab = normalize(ab, true);
+        ac = normalize(ac, true);
+        bc = normalize(bc, true);
+
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(ab, b, bc, count - 1);
+        divideTriangle(bc, c, ac, count - 1);
+        divideTriangle(ab, bc, ac, count - 1);
+    } else {
+        triangle(a, b, c);
+    }
+}
+
+
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
+}
 
 //立方体每个面6个顶点位置及法向量,先计算每个面的法向量，
 //然后，每个面分成两个三角形，按顺序分别将其顶点位置写入pointsArray数组，
@@ -192,6 +259,7 @@ window.onload = function init() {
     };
 
     colorCube();//立方体的所有顶点位置和法向量写入缓存
+    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
 
     var nBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
@@ -383,6 +451,9 @@ var render = function () {
     gl.depthMask(false);
     gl.drawArrays(gl.TRIANGLES, 36, numVertices);
     gl.depthMask(true);
+    for (var i = 72; i < index; i += 3) {
+        gl.drawArrays(gl.TRIANGLES, i, 3);
+    }
     requestAnimFrame(render);//双帧切换
 };
 
